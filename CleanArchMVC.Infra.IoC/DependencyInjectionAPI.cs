54740df39396c3fa221d17fe35a6a9974c220e96
Kaique <kaiqueamaral01@gmail.com>
2025-01-17 12:18:11 -0300

    Adicionado projeto de Web API

---
+++ CleanArchMVC.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMVC.Application.Interfaces;
+using CleanArchMVC.Application.Mappings;
+using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Account;
+using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using CleanArchMVC.Infra.Data.Identity;
+using CleanArchMVC.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMVC.Infra.IoC;
+
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+          configuration.GetConnectionString("DefaultConnection"),
+          b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+        //add scoped recomendado para web
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IProductService, ProductService>();
+
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        var myHandles = AppDomain.CurrentDomain.Load("CleanArchMVC.Application");
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(myHandles));
+
+        return services;
+    }
+}
